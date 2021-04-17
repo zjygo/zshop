90caaece5d8717ecc0ff3a5ff0ba2bde94cf8a39
zjygo <7882457@qq.com>
2021-04-17 17:05:16 +0800

    单元测试以及如何避免脏数据

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_register()
+    {
+        $response = $this->post('wx/auth/register', [
+            'username' => 'zjygo2',
+            'password' => '123456',
+            'mobile' => '18633392852',
+            'code' => '1234'
+        ]);
+        $response->assertStatus(200);
+        $ret = $response->getOriginalContent();
+        $this->assertEquals(0, $ret['errno']);
+        $this->assertNotEmpty($ret['data']);
+    }
+
+    public function test_register_mobile()
+    {
+        $response = $this->post('wx/auth/register', [
+            'username' => 'zjygo2',
+            'password' => '123456',
+            'mobile' => '18633392852',
+            'code' => '1234'
+        ]);
+        $response->assertStatus(200);
+        $ret = $response->getOriginalContent();
+        $this->assertEquals(707, $ret['errno']);
+    }
+}
